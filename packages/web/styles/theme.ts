0611aa79eefc5dd2881ebc91f8b86732f8769f94
bowen <xboxue@gmail.com>
2019-09-13 20:04:12 -0400

    Add Styled System theme

---
+++ packages/web/styles/theme.ts
@@ -0,0 +1,11 @@
+const scale = [0, 4, 8, 16, 24, 32, 48, 64, 96, 128, 192, 256, 384, 512, 640];
+
+export const theme = {
+  space: scale,
+  sizes: scale,
+  fontSizes: [12, 14, 16, 18, 20, 24, 30, 36, 48, 60, 72],
+  breakpoints: ["40em", "52em", "64em"],
+  colors: {
+    background: "hsl(0, 0%, 98%)"
+  }
+};
